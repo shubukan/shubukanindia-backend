4ba225f3dbdb23c0440e0d4d8c87d2427d7dd066
Tathagata Mishra <tathagatamishra007@gmail.com>
2025-02-04 00:17:06 +0530

    debug

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,36 +2,39 @@ const jwt = require('jsonwebtoken');
 const AdminModal = require('../model/adminModel');
 
 exports.authMiddleware = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization?.split(' ')[1];
+  // try {
+  //   const token = req.headers.authorization?.split(' ')[1];
     
-    if (!token) {
-      return res.status(401).json({
-        success: false,
-        error: 'No token provided'
-      });
-    }
+  //   if (!token) {
+  //     return res.status(401).json({
+  //       success: false,
+  //       error: 'No token provided'
+  //     });
+  //   }
 
-    const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    const admin = await AdminModal.findOne({ id: decoded.id });
+  //   const decoded = jwt.verify(token, process.env.SECRET_KEY);
+  //   const admin = await AdminModal.findOne({ id: decoded.id });
     
-    if (!admin) {
-      return res.status(401).json({
-        success: false,
-        error: 'Invalid token'
-      });
-    }
+  //   if (!admin) {
+  //     return res.status(401).json({
+  //       success: false,
+  //       error: 'Invalid token'
+  //     });
+  //   }
 
-    // Add last active timestamp
-    admin.lastActive = new Date();
-    await admin.save();
+  //   // Add last active timestamp
+  //   admin.lastActive = new Date();
+  //   await admin.save();
 
-    req.admin = admin;
-    next();
-  } catch (error) {
-    return res.status(401).json({
-      success: false,
-      error: 'Invalid token'
-    });
-  }
+  //   req.admin = admin;
+  //   next();
+  // } catch (error) {
+  //   return res.status(401).json({
+  //     success: false,
+  //     error: 'Invalid token'
+  //   });
+  // }
+
+  console.log('Auth Middleware - Headers:', req.headers);
+  next();
 };
