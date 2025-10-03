55106aae309a98bd2024995038da5bee1a1e3fed
Tathagata Mishra <tathagatamishra007@gmail.com>
2025-10-03 14:50:56 +0530

    update demo question action

--- model/questionModel.js
+++ model/questionModel.js
@@ -1,13 +1,26 @@
 // model/questionModel.js
 const mongoose = require("mongoose");
-
 const questionSchema = new mongoose.Schema(
   {
+    questionID: { type: Number, unique: true, index: true }, // sequential ID
     question: { type: String, required: true },
     options: [{ type: String, required: true }],
     answer: { type: Number, required: true }, // index of correct option
     questionSet: { type: Number, default: 1 },
     isDeleted: { type: Boolean, default: false },
+
+    // New fields:
+    used: { type: Boolean, default: false }, // true if used by any non-deleted exam
+    usedInExams: [
+      {
+        exam: { type: mongoose.Schema.Types.ObjectId, ref: "Exam" }, // exam _id
+        examID: { type: String }, // 6-char examID
+        examSet: { type: Number },
+        accessability: { type: String },
+        examDate: { type: Date },
+        addedAt: { type: Date, default: Date.now },
+      },
+    ],
   },
   { timestamps: true }
 );
